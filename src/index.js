147743fa8e93cf0544068f4bfffc964fa86e22db
Boris Reif <boris.reif@gmail.com>
2026-04-30 20:52:56 +0200

    Fix bug about package entry point

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { createBucketedCuckooMap } from "./cuckoo.js";
+export { createResizableMap } from "./createResizableMap.js";
+export { createStringNumberKeyOps } from "./keyops/stringNumberKeyOps.js";
+export { createByteKeyOps } from "./keyops/byteKeyOps.js";
+export { createDigestKeyOps } from "./keyops/digestKeyOps.js";
